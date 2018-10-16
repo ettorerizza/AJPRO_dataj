ff141f379321f1f98fc302e721ddedf5efefb805
Ettore Rizza <ettorerizza@gmail.com>
2018-10-16 20:42:00 +0200

    ajout du code ins des communes

--- 3_analyse_graphique/rate_lelection_dunfil.R
+++ 3_analyse_graphique/rate_lelection_dunfil.R
@@ -2,32 +2,20 @@ library(dplyr)
 library(readr)
 library(ggplot2)
 
-#ne faite pas attention à ces lignes de code
-#elles servent à définir automatiquement votre
-#répertoire de travail dans le bon dossier 
-set_wd <- function() {
-  library(rstudioapi) # à installer au besoin
-  current_path <- getActiveDocumentContext()$path 
-  setwd(dirname(current_path ))
-  print( getwd() )
-}
-set_wd()
-
 
 #On importe et visualise le fichier csv issu des scraping
-data = read_csv("scrapings_wallonie_bruxelles.csv")
-
-View(data)
-
+library(readr)
+data <- read_csv("C:/Users/ettor/Desktop/scraping_2018_wallonie_bruxelles_communales.csv")
+View(scraping_2018_wallonie_bruxelles_communales)
 
 #ajouter une colonne avec le nombre de voix du dernier élu
 #calculer la différence entre cette colonne et le nombre de voix des premiers suppléants
 rate_lelection = 
   data %>% 
   group_by(liste) %>% 
-  mutate(voix_last_elu = min(voix[which(elu=="oui")]),
-         difference = voix - voix_last_elu) %>% 
-  filter(elu=="non", difference==-1) 
+  mutate(voix_last_elu = min(Votes[which(elu=="oui")]),
+         difference = Votes - voix_last_elu) %>% 
+  filter(elu=="non", difference==0) 
 
 
 
