b333283e3932b75e3f815c827bfd7db80fbfbd20
Ettore Rizza <ettorerizza@gmail.com>
2018-10-18 12:51:14 +0200

    ajout de rate_lelection_dun_fil

--- 3_analyse_graphique/rate_lelection_dunfil.R
+++ 3_analyse_graphique/rate_lelection_dunfil.R
@@ -4,9 +4,8 @@ library(ggplot2)
 
 
 #On importe et visualise le fichier csv issu des scraping
-library(readr)
-data <- read_csv("C:/Users/ettor/Desktop/scraping_2018_wallonie_bruxelles_communales.csv")
-View(scraping_2018_wallonie_bruxelles_communales)
+data <- read_csv("GitHub/AJPRO_dataj/1_scraping/code/scraping_2018_wallonie_bruxelles_communales.csv")
+View(data)
 
 #ajouter une colonne avec le nombre de voix du dernier élu
 #calculer la différence entre cette colonne et le nombre de voix des premiers suppléants
@@ -15,11 +14,11 @@ rate_lelection =
   group_by(liste) %>% 
   mutate(voix_last_elu = min(Votes[which(elu=="oui")]),
          difference = Votes - voix_last_elu) %>% 
-  filter(elu=="non", difference==0) 
+  filter(elu=="non", difference<=0 & difference >=-3) 
 
 
 
 View(rate_lelection)
 
 #on sauvegarde les résultats
-write_csv(result, "rate_lelection.csv")
+write_excel_csv(rate_lelection, "rate_lelection.csv")
